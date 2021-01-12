256722d8598c325676489e5b12e36233c6d6cc7e
LucaMagdeburg <luca.magdeburg@fh-bielefeld.de>
2021-01-12 15:13:26 +0100

    added classes 'PwdServerSingleThread' and 'PwdServerMultiThread'

--- PwdServer.cpp
+++ PwdServer.cpp
@@ -6,15 +6,12 @@
  */
 
 #include <iostream>
+#include <stdio.h>
 #include <cstdlib>
 
 #include "PwdServer.hpp"
 
-PwdServer::PwdServer(int port, int maxDataSizeRecv, int pwdLength, int symbSetSize) :
-    TCPserver(port, maxDataSizeRecv){
-    ptrBBS_ = new TASK1::BlackBoxSafe(pwdLength, symbSetSize);
-    return;
-}
+PwdServer::PwdServer(int port, int maxDataSizeRecv) : TCPserver(port, maxDataSizeRecv){;};
 
 PwdServer::~PwdServer(){
     if(dataRecv_ != NULL){
@@ -48,25 +45,57 @@ string PwdServer::myResponse(string input){
     if(input.compare(0,15,"UPDATE PASSWORD") == 0){
         int newPwdLength, newSymbSetSize = 0;
 
-        try{
-            newPwdLength   = stoi(input.substr(16, (input.find(",", 16) - 16)));
-            newSymbSetSize = stoi(input.substr(input.find(",", 16) + 1));
-        }catch(...){
-            //cerr << "ERROR: can't read received password parameters." << endl;
+        if(!sscanf(input.c_str(), "UPDATE PASSWORD[%d,%d]", &newPwdLength, &newSymbSetSize)){
             return string("UNRECOGNISED PASSWORD PARAMETERS");
         }
+        
+        if( (newPwdLength < 1) || (newSymbSetSize < 1) || (newSymbSetSize > TASK1::SYMBOLS.length()) ){
+            return string("INVALID PASSWORD PARAMETERS");
+        }else{
+            if(ptrBBS_ != NULL) delete ptrBBS_;
 
-        if(ptrBBS_ != NULL) delete ptrBBS_;
+            ptrBBS_ = new TASK1::BlackBoxSafe(newPwdLength, newSymbSetSize);
 
-        ptrBBS_ = new TASK1::BlackBoxSafe(newPwdLength, newSymbSetSize);
-
-        return string("PASSWORD UPDATED");
+            return string("PASSWORD UPDATED");
+        }
     }
 
     return string(ptrBBS_->input(input));
 }
 
 void PwdServer::flush(){
-    delete [] dataRecv_;
-    dataRecv_ = new char[maxDataSizeRecv_];   
+    //delete [] dataRecv_;
+    //dataRecv_ = new char[maxDataSizeRecv_];
+
+    for(int i = 0; i < maxDataSizeRecv_; i++){
+        dataRecv_[i] = '\0';
+    }   
+
+    return;
+}
+
+
+PwdServerSingleThread::PwdServerSingleThread(int port, int maxDataSizeRecv, int pwdLength, int symbSetSize) :
+    PwdServer(port, maxDataSizeRecv){
+    ptrBBS_ = new TASK1::BlackBoxSafe(pwdLength, symbSetSize);
+    return;
+}
+
+PwdServerMultiThread::PwdServerMultiThread(int port, int maxDataSizeRecv, int pwdLength, int SymbSetSize, int threadCount = 1){
+    ptrBBS_ = new TASK1::BlackBoxSafe(pwdLength, SymbSetSize);
+
+    pwdServers_ = new PwdServer*[threadCount];
+    serverThreads_ = new std::thread*[threadCount];
+
+    for(int i = 0; i < threadCount; i++){
+        pwdServers_[i] = new PwdServer((port + i), maxDataSizeRecv);
+    }
+}
+
+void PwdServerMultiThread::startServer(){
+    for(int i = 0; i < threadCount_; i++){
+        serverThreads_[i] = new std::thread(&TCPserver::run, ref(pwdServers_[i]));
+    }
+
+
 }
--- PwdServer.hpp
+++ PwdServer.hpp
@@ -9,6 +9,8 @@
 #ifndef PwdServer_hpp_
 #define PwdServer_hpp_
 
+#include <thread>
+
 #include "SIMPLESOCKET.hpp"
 #include "TASK1.hpp"
 
@@ -17,8 +19,8 @@ class PwdServer : public TCPserver{
         TASK1::BlackBoxSafe *ptrBBS_; 
 
     public:
-             PwdServer(int port, int maxDataSizeRecv, int pwdLength, int symbSetSize);
-            ~PwdServer();
+        PwdServer(int port, int maxDataSizeRecv);
+        ~PwdServer();
 
     protected:
         string myResponse(string input);
@@ -28,5 +30,28 @@ class PwdServer : public TCPserver{
         string response(string incomingMsg);
 
 };
+
+
+class PwdServerSingleThread : public PwdServer{
+    public: 
+        PwdServerSingleThread(int port, int maxDataSizeRecv, int pwdLength, int SymbSetSize);
+};
+
+class PwdServerMultiThread{
+    protected:
+        int threadCount_;
+        int port_;
+        int maxDataSizeRecv_;
+        TASK1::BlackBoxSafe *ptrBBS_;
+
+        PwdServer **pwdServers_;
+        std::thread **serverThreads_;
+
+    public:
+        PwdServerMultiThread(int port, int maxDataSizeRecv, int pwdLength, int SymbSetSize, int threadCount);
+        ~PwdServerMultiThread();
+        void startServer();
+};
+
 
 #endif /* PwdServer_hpp_ */
